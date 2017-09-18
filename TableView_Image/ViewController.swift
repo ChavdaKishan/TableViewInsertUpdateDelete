7d5a347f10446eae760c20097c945cbb28089f8b
iFlame <iflame@rd.local>
2017-09-18 13:29:00 +0530

    1

--- TableView_Image/ViewController.swift
+++ TableView_Image/ViewController.swift
@@ -45,15 +45,14 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         if editingStyle == .delete
         {
             arr.remove(at: indexPath.row)
-            /*tableView.deleteRows(at: [indexPath], with: .automatic)
-            NSLog("Row %i Deleted", indexPath.row)*/
             tableView.reloadData()
         }
     }*/
+    
     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]?
     {
-        let edit = UITableViewRowAction(style: .default, title: "Edit") { (action, indexPath) in
-            let alert = UIAlertController(title: "Edit", message: "Enter:", preferredStyle: .alert)
+        let Update = UITableViewRowAction(style: .default, title: "Update") { (action, indexPath) in
+            let alert = UIAlertController(title: "Update", message: "Enter:", preferredStyle: .alert)
             alert.addTextField(configurationHandler: nil)
             alert.addAction(UIAlertAction(title: "Update", style: .default, handler: { (updateAction) in
                 self.arr[indexPath.row] = (alert.textFields?.first?.text)!
@@ -62,13 +61,15 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
             alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
             self.present(alert,animated: false)
         }
+        
         let delete = UITableViewRowAction(style: .default, title: "delete")
         {
             action,indexPath in
             self.arr.remove(at: indexPath.row)
             self.TableView.reloadData()
         }
-        return [edit,delete]
+        
+        return [Update,delete]
     }
     
     /*func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle
@@ -81,17 +82,14 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
         
     }*/
-
+    
     @IBAction func AddClick(_ sender: Any)
     {
         if TextField.text != ""
         {
             arr.append(TextField.text!)
-            /*let index = IndexPath(row: arr.count - 1, section: 0)
-             TableView.insertRows(at: [index], with: .automatic)*/
             TextField.text = ""
             TableView.reloadData()
         }
-//        TableView.setEditing(true, animated: true)
     }
 }
